71e698664c006d148d6f18437e9c22f368e914d1
Yeasir0032 <sdyeasirrahaman@gmail.com>
2024-07-31 17:31:39 +0530

    Added cors

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 5000;
 //Eastablish connection for mongoose
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
 app.listen(port, () => {
