479bd24e7dfd321834db4b353f3621116db58d0c
Yeasir0032 <sdyeasirrahaman@gmail.com>
2024-07-31 18:21:30 +0530

    Added cors to frontend

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://workflo-frontend.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
